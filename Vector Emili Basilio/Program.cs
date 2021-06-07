f2a9eb2dd3d8f5fbd6e29c3c26d52fa9ac922bd9
emilibasilio <85315420+emilibasilio@users.noreply.github.com>
2021-06-07 12:27:42 -0600

    Add files via upload

---
+++ Vector Emili Basilio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Vector_Emili_Basilio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] dato = new char[10];
+
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.WriteLine("i:" + i);
+                Console.Write("ngrese una letra:");
+                dato[i] = char.Parse(Console.ReadLine());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
